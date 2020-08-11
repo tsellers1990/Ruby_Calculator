f0fc6fe072bb53a3081d560f61581341551f90ff
tsellers1990 <tsellers1990@gmail.com>
2020-08-11 13:30:59 -0600

    added an addition feature

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,14 @@ puts "enter your name:"
 name = gets 
 
 puts (("hello ") + name + "This is a demo Ruby calculator. What's the first number?")
-num1 = gets
-puts
+num1 = gets.chomp().to_f
+
+puts "Great! What's the second number?"
+num2 = gets.chomp().to_f
+
+sum = (num1 + num2).to_s
+
+puts (
+    ("The Sum of those two numbers is " + sum ) +
+    ("\nThe ")
+)
