36699bb74f034d240722aff734b88aa537f80228
tsellers1990 <tsellers1990@gmail.com>
2020-08-11 13:33:53 -0600

    added multiple, division, and modulus

--- calculator.rb
+++ calculator.rb
@@ -8,8 +8,13 @@ puts "Great! What's the second number?"
 num2 = gets.chomp().to_f
 
 sum = (num1 + num2).to_s
+multiple = (num1 * num2).to_s
+division = (num1 / num2).to_s
+modulus = (num1 % num2).to_s
 
 puts (
     ("The Sum of those two numbers is " + sum ) +
-    ("\nThe ")
+    ("\nThe Multiple of those two numbers is " + multiple ) + 
+    ("\nThe Division of those two numbers is " + division ) + 
+    ("\nThe Modulus of those two numbers is " + modulus )
 )
